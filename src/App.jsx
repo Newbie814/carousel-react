8f0ccee5098635ed5681ef14239c7db5823ee1f2
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-31 21:11:21 -0400

    initialization of proj and initial carousel layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+import Carousel from './components/Carousel';
+// import { data } from './data';
+
+const App = () => {
+  const [] = useState();
+  return (
+    <main>
+      <Carousel />
+    </main>
+  );
+};
+export default App;
