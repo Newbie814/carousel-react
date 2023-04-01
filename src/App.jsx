7ceb9ab7a3cc4ab5567794f3fadb9f58c56d64af
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-03-31 22:50:43 -0400

    cleaned out unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import Carousel from './components/Carousel';
 // import { data } from './data';
 
 const App = () => {
-  const [] = useState();
   return (
     <main>
       <Carousel />
